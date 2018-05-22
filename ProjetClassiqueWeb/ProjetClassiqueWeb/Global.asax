ec8988df3662d42ab119c2e6efcb89f66b7199b5
Vincent Picot <Vincent@info-galen.iut.bx1>
2018-05-22 15:56:22 +0200

    Création

---
+++ ProjetClassiqueWeb/ProjetClassiqueWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetClassiqueWeb.MvcApplication" Language="C#" %>
---
+++ ProjetClassiqueWeb/ProjetClassiqueWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetClassiqueWeb.Startup))]
+namespace ProjetClassiqueWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
